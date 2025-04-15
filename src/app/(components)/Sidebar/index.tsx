9fbe9e32d2c4c2d5bf21458fa424c0e8eec3b851
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-04-15 20:31:21 +0600

    Create index.tsx

---
+++ src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="flex gap-3 justify-between md:justify-normal pt-8">
+      <div>Logo</div>
+      <h1 className={`font-extrabold text-2xl`}>Shakil</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
